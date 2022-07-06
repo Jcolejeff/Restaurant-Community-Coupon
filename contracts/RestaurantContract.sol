1b7f6baa9bcbf247e3c3b928c21a7d277ad1a2f2
FarzeenKist <farzeenkistnareddy@gmail.com>
2022-07-06 20:45:33 +0400

    optimized smart contract and added new features

--- contracts/RestaurantContract.sol
+++ contracts/RestaurantContract.sol
@@ -11,13 +11,16 @@ contract RestaurantNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
+    Counters.Counter private _restaurantCounter;
 
     constructor() ERC721("MR BIGGS", "BIGGS") {}
 
     uint256 owners = 0;
+    // fee to create a restaurant
+    uint256 fee = 1 ether;
 
-    struct Restaurant {
-        uint256 tokenId;
+    struct Coupon {
+        uint256 restaurantId;
         string name;
         string description;
         address payable seller;
@@ -26,27 +29,162 @@ contract RestaurantNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         bool sold;
     }
 
-    mapping(uint256 => Restaurant) private restaurants;
+    struct Restaurant {
+        string name;
+        string description;
+        address owner;
+        uint256 availableCoupons;
+        uint256 soldCoupons;
+        uint256 totalCoupons;
+        uint256 admins;
+    }
 
-    function safeMint(string memory uri, string memory _name, string memory _description, uint256 price)
+    mapping(uint256 => Restaurant) public restaurants;
+
+    mapping(uint256 => bool) private restaurantExists;
+    mapping(uint256 => mapping(address => bool)) public restaurantAdmins;
+
+    mapping(uint256 => Coupon) public coupons;
+
+    modifier checkInputs(string memory _name, string memory _description) {
+        require(bytes(_name).length > 0, "Invalid name");
+        require(bytes(_description).length > 0, "Invalid description");
+
+        _;
+    }
+
+    modifier validateRestaurantOwner(uint256 restaurantId, address admin) {
+        require(
+            restaurants[restaurantId].owner == msg.sender,
+            "Unauthorized user"
+        );
+        require(admin != address(0), "Invalid address");
+        _;
+    }
+    modifier verifyPrice(uint256 price) {
+        require(price > 0, "Invalid price");
+        _;
+    }
+
+    modifier exist(uint256 tokenId) {
+        require(_exists(tokenId), "query of non existent token");
+        _;
+    }
+
+    modifier existRestaurant(uint256 restaurantId) {
+        require(
+            restaurantExists[restaurantId],
+            "Query of non existent restaurant"
+        );
+        _;
+    }
+
+    modifier ownedAndSold(uint256 tokenId) {
+        require(
+            coupons[tokenId].owner == msg.sender,
+            "Only item owner can perform this operation"
+        );
+        require(
+            coupons[tokenId].sold &&
+                coupons[tokenId].seller == address(0) &&
+                coupons[tokenId].price == 0,
+            "coupon is already sale"
+        );
+        _;
+    }
+
+    function createRestaurant(string memory _name, string memory _description)
         public
         payable
+        checkInputs(_name, _description)
+    {
+        require(msg.value == fee, "Payment of fee is required");
+        uint256 id = _restaurantCounter.current();
+        _restaurantCounter.increment();
+        restaurants[id] = Restaurant(
+            _name,
+            _description,
+            msg.sender,
+            0,
+            0,
+            0,
+            0
+        );
+        restaurantAdmins[id][msg.sender] = true;
+        restaurantExists[id] = true;
+    }
+
+    // add admin to restaurant
+    // remove admin from restaurant
+    function addRestaurantAdmin(uint256 restaurantId, address admin)
+        public
+        validateRestaurantOwner(restaurantId, admin)
+    {
+        require(!restaurantAdmins[restaurantId][admin], "already an admin");
+        restaurantAdmins[restaurantId][admin] = true;
+        restaurants[restaurantId].admins++;
+    }
+
+    // remove admin of a restaurant
+    // callable only by the restaurant owner
+    function removeRestaurantAdmin(uint256 restaurantId, address admin)
+        public
+        validateRestaurantOwner(restaurantId, admin)
+    {
+        require(
+            restaurantAdmins[restaurantId][admin],
+            "address isn't an admin"
+        );
+        restaurantAdmins[restaurantId][admin] = false;
+        restaurants[restaurantId].admins--;
+    }
+
+    // allows a restaurant to add a coupon
+    // calleable only by a restaurant admin or the restaurant owner
+    function safeMint(
+        string memory uri,
+        string memory _name,
+        string memory _description,
+        uint256 price,
+        uint256 restaurantId
+    )
+        public
+        payable
+        existRestaurant(restaurantId)
+        checkInputs(_name, _description)
+        verifyPrice(price)
         returns (uint256)
     {
+        require(bytes(uri).length > 0, "Invalid uri");
+        require(
+            restaurants[restaurantId].owner == msg.sender ||
+                restaurantAdmins[restaurantId][msg.sender],
+            "Only restaurants owners or admins can mint a coupon"
+        );
         uint256 tokenId = _tokenIdCounter.current();
+
         _tokenIdCounter.increment();
+        addRestaurantCoupon(tokenId, restaurantId, _name, _description, price);
+        restaurants[restaurantId].totalCoupons++;
+        restaurants[restaurantId].availableCoupons++;
+
         _mint(msg.sender, tokenId);
 
         _setTokenURI(tokenId, uri);
-        addRestaurantCoupon(tokenId, _name, _description, price);
+
         return tokenId;
     }
 
-
-    function addRestaurantCoupon(uint256 tokenId, string memory _name, string memory _description, uint256 price) private {
-        require(price > 0, "Price must be at least 1 wei");
-        restaurants[tokenId] = Restaurant(
-            tokenId,
+    function addRestaurantCoupon(
+        uint256 tokenId,
+        uint256 restaurantId,
+        string memory _name,
+        string memory _description,
+        uint256 price
+    ) private checkInputs(_name, _description) verifyPrice(price) {
+        require(!_exists(tokenId), "token already exists");
+        coupons[tokenId] = Coupon(
+            restaurantId,
             _name,
             _description,
             payable(msg.sender),
@@ -54,39 +192,59 @@ contract RestaurantNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
             price,
             false
         );
-
         _transfer(msg.sender, address(this), tokenId);
     }
 
-    function buyCoupon(uint256 tokenId) public payable {
-        uint256 price = restaurants[tokenId].price;
-        address seller = restaurants[tokenId].seller;
+    // allows users to buy a coupon from a restaurant
+    function buyCoupon(uint256 tokenId) public payable exist(tokenId) {
+        uint256 price = coupons[tokenId].price;
+        address seller = coupons[tokenId].seller;
         require(
-            msg.value >= price,
+            msg.value == price,
             "Please submit the asking price in order to complete the purchase"
         );
-        restaurants[tokenId].owner = payable(msg.sender);
-        restaurants[tokenId].sold = true;
-        restaurants[tokenId].seller = payable(address(0));
+        require(
+            seller != msg.sender,
+            "coupon owners can't buy their own tokens"
+        );
+        require(!coupons[tokenId].sold, "coupon isn't available");
+        coupons[tokenId].owner = payable(msg.sender);
+        coupons[tokenId].sold = true;
+        coupons[tokenId].seller = payable(address(0));
+        coupons[tokenId].price = 0;
+        restaurants[coupons[tokenId].restaurantId].soldCoupons++;
+        restaurants[coupons[tokenId].restaurantId].availableCoupons++;
         _transfer(address(this), msg.sender, tokenId);
-
-        payable(seller).transfer(msg.value);
+        (bool success, ) = payable(seller).call{value: price}("");
+        require(success, "payment failed");
     }
 
-    function sellCoupon(uint256 tokenId) public payable {
-        require(
-            restaurants[tokenId].owner == msg.sender,
-            "Only item owner can perform this operation"
-        );
-        restaurants[tokenId].sold = false;
-        restaurants[tokenId].seller = payable(msg.sender);
-        restaurants[tokenId].owner = payable(address(this));
-
+    // allows user to sell a coupon from a restaurant
+    function sellCoupon(uint256 tokenId, uint256 price)
+        public
+        payable
+        exist(tokenId)
+        ownedAndSold(tokenId)
+        verifyPrice(price)
+    {
+        Coupon storage currentCoupon = coupons[tokenId];
+        currentCoupon.sold = false;
+        currentCoupon.seller = payable(msg.sender);
+        currentCoupon.owner = payable(address(this));
+        currentCoupon.price = price;
+        restaurants[currentCoupon.restaurantId].soldCoupons--;
+        restaurants[currentCoupon.restaurantId].availableCoupons++;
         _transfer(msg.sender, address(this), tokenId);
     }
 
-    function getCoupons(uint256 tokenId) public view returns (Restaurant memory) {
-        return restaurants[tokenId];
+    // allows user to claim and burn a coupon
+    function claimCoupon(uint256 tokenId)
+        public
+        exist(tokenId)
+        ownedAndSold(tokenId)
+    {
+        delete coupons[tokenId];
+        _burn(tokenId);
     }
 
     function getCouponsLength() public view returns (uint256) {
